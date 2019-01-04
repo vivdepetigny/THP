0f2f67cf3776528b59c0eaffcfe7a0df6e5e7dcc
vivdepetigny <46379769+vivdepetigny@users.noreply.github.com>
2019-01-04 17:54:56 +0100

    Ruby Ex3

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts " $ ruby exo_01.rb"
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+#Explication : la ligne de code est désactivée et passée en commentaire, le code ne produit donc que "Bonjour, monde !"
