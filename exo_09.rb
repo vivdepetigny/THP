b4c17de8420ae50af67298bc0b5c267862d3f998
vivdepetigny <46379769+vivdepetigny@users.noreply.github.com>
2019-01-04 17:58:54 +0100

    Ruby Ex9

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton prénom ?"
+print "> "
+first_name = gets.chomp
+puts "Et ton nom de famille ?"
+print "> "
+last_name = gets.chomp
+puts "Bonjour, " + first_name + " " + last_name
