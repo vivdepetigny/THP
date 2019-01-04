8760dd60d798d01bf44ddeecda9bb50c9075c449
vivdepetigny <46379769+vivdepetigny@users.noreply.github.com>
2019-01-04 17:57:57 +0100

    Ruby Ex7

---
+++ exo_07.rb
@@ -0,0 +1,15 @@
+#Exo_07a.
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#Exo_07b.
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#Exo_07c.
+user_name = gets.chomp
+puts user_name
