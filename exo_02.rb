8e5860f00e10e0239cf5ed3957e1591b2672cb26
vivdepetigny <46379769+vivdepetigny@users.noreply.github.com>
2019-01-04 17:54:27 +0100

    Ruby Ex2

--- exo_02.rb
+++ exo_02.rb
@@ -2,4 +2,5 @@ puts " $ ruby exo_01.rb"
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
 
+
 #Différence entre pruts et print : avec la commande "puts" la production revient à la ligne après lecture du code, contrairement à "print"
